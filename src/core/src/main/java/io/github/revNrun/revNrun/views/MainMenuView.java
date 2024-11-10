449da4612893cf6816f3e4675f6f149e9760ef26
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-10 01:46:41 +0100

    Forgot fixing this error

--- src/core/src/main/java/io/github/revNrun/revNrun/views/MainMenuView.java
+++ src/core/src/main/java/io/github/revNrun/revNrun/views/MainMenuView.java
@@ -1,6 +1,12 @@
 package io.github.revNrun.revNrun.views;
 
+import com.badlogic.gdx.utils.viewport.Viewport;
+
 public class MainMenuView extends View {
+    public MainMenuView(Viewport viewport) {
+        super(viewport);
+    }
+
     @Override
     public void render() {
 
