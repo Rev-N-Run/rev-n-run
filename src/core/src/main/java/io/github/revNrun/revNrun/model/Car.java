b1100f75fc65eb001767360eff2809bfcdd1e818
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-13 01:44:53 +0100

    DEVELOPMENT: Declaration of minimal variables needed for test

---
+++ src/core/src/main/java/io/github/revNrun/revNrun/model/Car.java
@@ -0,0 +1,16 @@
+package io.github.revNrun.revNrun.model;
+
+public class Car {
+    private int speed;
+    private int maxSpeed;
+    private int fuel;
+    private Engine engine;
+    private Chasis chasis;
+    private Tires tires;
+    private Suspension suspension;
+    private Brakes brakes;
+    private Floor floor;
+    private Front front;
+    private Back back;
+    private Sides sides;
+}
