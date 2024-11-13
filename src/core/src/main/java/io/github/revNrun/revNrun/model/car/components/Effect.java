f8dced3cbc005f73bf9a19a2e7ac45f0ac9b9718
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-13 19:54:11 +0100

    DEVELOPMENT: Implemented logic in Effect constructor to only allow the min and max values set.

--- src/core/src/main/java/io/github/revNrun/revNrun/model/car/components/Effect.java
+++ src/core/src/main/java/io/github/revNrun/revNrun/model/car/components/Effect.java
@@ -1,12 +1,17 @@
 package io.github.revNrun.revNrun.model.car.components;
 
 public class Effect {
+    private static final float MAX_VALUE = 20f;
+    private static final float MIN_VALUE = -20f;
+
     private final EffectType effect;
     private final float value;
 
     public Effect(EffectType effect, float value) {
         this.effect = effect;
-        this.value = value;
+        if (value < MIN_VALUE) {
+            this.value = MIN_VALUE;
+        } else this.value = Math.min(value, MAX_VALUE);
     }
 
     public EffectType getEffect() {
