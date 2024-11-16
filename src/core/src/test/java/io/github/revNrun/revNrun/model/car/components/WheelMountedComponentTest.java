cd71b876b3ab4ff6154f6b04f04630373e13f305
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-16 20:48:45 +0100

    TEST: fix of previous test

--- src/core/src/test/java/io/github/revNrun/revNrun/model/car/components/WheelMountedComponentTest.java
+++ src/core/src/test/java/io/github/revNrun/revNrun/model/car/components/WheelMountedComponentTest.java
@@ -62,7 +62,7 @@ class WheelMountedComponentTest {
     @Test
     void testDegrade() {
         float percentage = 0.5f;
-        float expected = wheelMountedComponent.getMaxDurability() / 2;
+        float expected = TEST_CURRENT_DURABILITY * (1 - TEST_WEAR_FACTOR * percentage);
         wheelMountedComponent.degrade(TEST_DELTA, percentage);
         assertEquals(expected, wheelMountedComponent.getMaxDurability());
     }
