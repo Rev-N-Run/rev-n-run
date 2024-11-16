eb18f764c0fbc8324d41bf8d08c8170ccbac4fdd
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-16 02:36:02 +0100

    DEV: Ensure that wear factor is also not 0

--- src/core/src/main/java/io/github/revNrun/revNrun/model/car/components/AbstractComponent.java
+++ src/core/src/main/java/io/github/revNrun/revNrun/model/car/components/AbstractComponent.java
@@ -18,7 +18,7 @@ public abstract class AbstractComponent implements Component {
         this.maxDurability = maxDurability;
         this.currentDurability = currentDurability;
         this.effects = (effects == null) ? new ArrayList<>() : effects;
-        if (wearFactor >= 1 || wearFactor < 0) {
+        if (wearFactor >= 1 || wearFactor <= 0) {
             throw new IllegalArgumentException("Wear factor cannot be greater or equal than 1");
         }
         this.wearFactor = wearFactor;
