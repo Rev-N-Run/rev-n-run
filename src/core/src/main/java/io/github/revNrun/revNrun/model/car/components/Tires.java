47697bd06219799e788118933fb35bcfc150baa0
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-26 19:45:42 +0100

    DEV: degrade grip on tires

--- src/core/src/main/java/io/github/revNrun/revNrun/model/car/components/Tires.java
+++ src/core/src/main/java/io/github/revNrun/revNrun/model/car/components/Tires.java
@@ -2,13 +2,24 @@ package io.github.revNrun.revNrun.model.car.components;
 
 import io.github.revNrun.revNrun.model.car.components.enums.CarAxis;
 import io.github.revNrun.revNrun.model.car.components.enums.CarSides;
+import io.github.revNrun.revNrun.model.car.components.enums.EffectType;
 
 import java.util.List;
 
 public class Tires extends WheelMountedComponent {
-
     public Tires(String name, float weight, float maxDurability, float currentDurability,
                  List<Effect> effects, CarAxis axle, CarSides side, float wearFactor) {
         super(name, weight, maxDurability, currentDurability, effects, axle, side, wearFactor);
+    }
+
+    @Override
+    public void degrade(float delta, float percentage) {
+        currentDurability *= (1 - wearFactor * delta * percentage);
+        for (Effect effect : effects) {
+            if (effect.getEffect() == EffectType.GRIP) {
+                float grip = effect.getValue() * (currentDurability / 100);
+                effect.setValue(grip);
+            }
+        }
     }
 }
