2e7f8efa8914dae01d3918120c52d3b168232b7a
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-28 18:30:00 +0100

    DEV: Record ghost car state (position, angle and timestamp)

--- src/core/src/main/java/io/github/revNrun/revNrun/model/ghost_car/GhostCar.java
+++ src/core/src/main/java/io/github/revNrun/revNrun/model/ghost_car/GhostCar.java
@@ -14,22 +14,54 @@ public class GhostCar {
     }
 
     public float getPositionX(int index) {
-        GhostState state = states.get(index);
+        GhostState state;
+        if (index < 0) {
+            state = states.getFirst();
+            return state.getX();
+        } else if (index >= states.size()) {
+            state = states.getLast();
+            return state.getX();
+        }
+        state = states.get(index);
         return state.getX();
     }
 
     public float getPositionY(int index) {
-        GhostState state = states.get(index);
+        GhostState state;
+        if (index < 0) {
+            state = states.getFirst();
+            return state.getY();
+        } else if (index >= states.size()) {
+            state = states.getLast();
+            return state.getY();
+        }
+        state = states.get(index);
         return state.getY();
     }
 
     public float getAngle(int index) {
-        GhostState state = states.get(index);
+        GhostState state;
+        if (index < 0) {
+            state = states.getFirst();
+            return state.getAngle();
+        } else if (index >= states.size()) {
+            state = states.getLast();
+            return state.getAngle();
+        }
+        state = states.get(index);
         return state.getAngle();
     }
 
     public float getTimestamp(int index) {
-        GhostState state = states.get(index);
+        GhostState state;
+        if (index < 0) {
+            state = states.getFirst();
+            return state.getTimestamp();
+        } else if (index >= states.size()) {
+            state = states.getLast();
+            return state.getTimestamp();
+        }
+        state = states.get(index);
         return state.getTimestamp();
     }
 }
