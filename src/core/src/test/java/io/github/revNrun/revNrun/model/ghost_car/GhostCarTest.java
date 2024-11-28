9507c30b302386414321b24aa02a77209651afef
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-28 18:22:12 +0100

    TEST: Record ghost car state (position, angle and timestamp)

--- src/core/src/test/java/io/github/revNrun/revNrun/model/ghost_car/GhostCarTest.java
+++ src/core/src/test/java/io/github/revNrun/revNrun/model/ghost_car/GhostCarTest.java
@@ -1,7 +1,19 @@
 package io.github.revNrun.revNrun.model.ghost_car;
 
+import io.github.revNrun.revNrun.controllers.vectorController.Vector2;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GhostCarTest {
+    @Test
+    void recordTest() {
+        GhostCar ghostCar = new GhostCar();
+        ghostCar.recordState(new Vector2(1, 1), 1, 1);
 
+        assertEquals(1, ghostCar.getPositionX(0));
+        assertEquals(1, ghostCar.getPositionY(0));
+        assertEquals(1, ghostCar.getAngle(0));
+        assertEquals(1, ghostCar.getTimestamp(0));
+    }
 }
