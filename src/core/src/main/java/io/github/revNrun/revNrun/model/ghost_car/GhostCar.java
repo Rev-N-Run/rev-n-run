44694b2535eba5540808d90640b0ea3df4f265fd
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-28 12:00:31 +0100

    TEST: Start of the test of the ghost car

---
+++ src/core/src/main/java/io/github/revNrun/revNrun/model/ghost_car/GhostCar.java
@@ -0,0 +1,4 @@
+package io.github.revNrun.revNrun.model.ghost_car;
+
+public class GhostCar {
+}
---
+++ src/core/src/test/java/io/github/revNrun/revNrun/model/ghost_car/GhostCarTest.java
@@ -0,0 +1,7 @@
+package io.github.revNrun.revNrun.model.ghost_car;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GhostCarTest {
+
+}
