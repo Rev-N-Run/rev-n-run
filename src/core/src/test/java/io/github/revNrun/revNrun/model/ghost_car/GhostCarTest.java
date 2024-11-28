c34ca9f8bd2ffecf87f14831e6a42cb07a5a32e3
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-28 18:26:32 +0100

    DEV: Record ghost car state (position, angle and timestamp)

--- src/core/src/test/java/io/github/revNrun/revNrun/model/ghost_car/GhostCarTest.java
+++ src/core/src/test/java/io/github/revNrun/revNrun/model/ghost_car/GhostCarTest.java
@@ -15,5 +15,20 @@ class GhostCarTest {
         assertEquals(1, ghostCar.getPositionY(0));
         assertEquals(1, ghostCar.getAngle(0));
         assertEquals(1, ghostCar.getTimestamp(0));
+
+        // Check out of index access
+        // < 0
+        assertEquals(ghostCar.getPositionX(0), ghostCar.getPositionX(-1));
+        assertEquals(ghostCar.getPositionY(0), ghostCar.getPositionY(-1));
+        assertEquals(ghostCar.getAngle(0), ghostCar.getAngle(-1));
+        assertEquals(ghostCar.getTimestamp(0), ghostCar.getTimestamp(-1));
+
+        ghostCar.recordState(new Vector2(2, 2), 2, 2);
+
+        // >= states.size()
+        assertEquals(ghostCar.getPositionX(1), ghostCar.getPositionX(1));
+        assertEquals(ghostCar.getPositionY(1), ghostCar.getPositionY(1));
+        assertEquals(ghostCar.getAngle(1), ghostCar.getAngle(1));
+        assertEquals(ghostCar.getTimestamp(1), ghostCar.getTimestamp(1));
     }
 }
