eec7631bf388561ad97c599cf05497ea485bc8e2
ferran sogas <0ferransogas4@gmail.com>
2024-11-29 22:02:30 +0100

    TEST: TrackControllerTest with TrackView mockito mock

---
+++ src/core/src/main/java/io/github/revNrun/revNrun/view/TrackView.java
@@ -0,0 +1,14 @@
+package io.github.revNrun.revNrun.view;
+
+import io.github.revNrun.revNrun.model.vector.Vector2;
+
+import java.util.List;
+
+public class TrackView {
+    public void drawTrack(List<Vector2> vertices){
+
+    }
+   public void drawLifeByCheckPoints(int lifeByCheckPoints){
+
+   }
+}
