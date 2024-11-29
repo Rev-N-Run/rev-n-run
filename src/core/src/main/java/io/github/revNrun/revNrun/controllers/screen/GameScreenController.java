d7e3f305edfcc3fb9016a50b8b79072a820c8d4d
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-29 10:21:21 +0100

    Created and initialized minimally the controller and view

--- src/core/src/main/java/io/github/revNrun/revNrun/controllers/screen/GameScreenController.java
+++ src/core/src/main/java/io/github/revNrun/revNrun/controllers/screen/GameScreenController.java
@@ -4,11 +4,15 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import io.github.revNrun.revNrun.Game;
+import io.github.revNrun.revNrun.controllers.car.CarController;
 import io.github.revNrun.revNrun.view.GameView;
 
-public class GameController extends ScreenController {
-    public GameController(Game game, SpriteBatch batch, Viewport viewport, Camera camera) {
+public class GameScreenController extends ScreenController {
+    private CarController carController;
+
+    public GameScreenController(Game game, SpriteBatch batch, Viewport viewport, Camera camera) {
         super(game);
         view = new GameView(viewport, camera, batch);
+        carController = new CarController();
     }
 }
