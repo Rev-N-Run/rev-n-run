72f00eade1141dcc04fd00d510408324a97561bb
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-30 00:18:38 +0100

    DEV: CarController execute

--- src/core/src/main/java/io/github/revNrun/revNrun/controllers/game/car/CarController.java
+++ src/core/src/main/java/io/github/revNrun/revNrun/controllers/game/car/CarController.java
@@ -1,15 +1,25 @@
 package io.github.revNrun.revNrun.controllers.game.car;
 
+import io.github.revNrun.revNrun.controllers.input.InputHandler;
+import io.github.revNrun.revNrun.controllers.input.LibGDXInputHelper;
 import io.github.revNrun.revNrun.model.car.Car;
 
 public class CarController {
+    private InputHandler input;
     private Car car;
 
-    public CarController(Car car) {
+    public CarController(Car car, InputHandler input) {
         this.car = car;
+        this.input = input;
     }
 
     public void execute(float delta) {
+        if (input.isUpPressed()) {
+            car.accelerate(delta);
+        } else if (input.isDownPressed()) {
+            car.brakeAndReverse(delta);
+        }
 
+        car.updatePosition(delta);
     }
 }
