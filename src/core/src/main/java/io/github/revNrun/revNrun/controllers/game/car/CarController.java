57ad400370aef956e8b6e1f46c2f2d6bbc5727aa
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-29 22:05:27 +0100

    Maybe a test?

--- src/core/src/main/java/io/github/revNrun/revNrun/controllers/game/car/CarController.java
+++ src/core/src/main/java/io/github/revNrun/revNrun/controllers/game/car/CarController.java
@@ -1,5 +1,15 @@
 package io.github.revNrun.revNrun.controllers.game.car;
 
-public class CarController {
+import io.github.revNrun.revNrun.model.car.Car;
 
+public class CarController {
+    private Car car;
+
+    public CarController(Car car) {
+        this.car = car;
+    }
+
+    public void execute(float delta) {
+
+    }
 }
---
+++ src/core/src/test/java/io/github/revNrun/revNrun/controllers/game/car/CarControllerTest.java
@@ -0,0 +1,12 @@
+package io.github.revNrun.revNrun.controllers.game.car;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarControllerTest {
+
+    @Test
+    void execute() {
+    }
+}
