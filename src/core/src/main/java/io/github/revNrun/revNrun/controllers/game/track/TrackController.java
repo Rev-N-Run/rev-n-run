8a196be1766821b801725c8bf8a5772fa65dbd3f
ferran sogas <0ferransogas4@gmail.com>
2024-11-29 19:10:38 +0100

    DEV: track controller

---
+++ src/core/src/main/java/io/github/revNrun/revNrun/controllers/game/track/TrackController.java
@@ -0,0 +1,25 @@
+package io.github.revNrun.revNrun.controllers.game.track;
+
+import io.github.revNrun.revNrun.model.track.Track;
+import io.github.revNrun.revNrun.model.vector.Vector2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrackController {
+
+    public TrackController() {
+    }
+
+    public void updateCarPosition(Vector2 carPos) {
+    }
+
+    public boolean isCarInTrack() {
+    }
+
+    public void draw() {
+    }
+
+    private void createVertices() {
+    }
+}
