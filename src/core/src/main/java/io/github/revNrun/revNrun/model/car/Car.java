0b717abbb4d52d8164431592a071e998d71c0699
Leonardo Edenak <leonardoedenak@gmail.com>
2024-11-30 01:28:27 +0100

    DEV: Car doesn't turn if there's no speed

--- src/core/src/main/java/io/github/revNrun/revNrun/model/car/Car.java
+++ src/core/src/main/java/io/github/revNrun/revNrun/model/car/Car.java
@@ -87,7 +87,7 @@ public class Car {
     }
 
     public void moveRight(float delta) {
-        if (tireGrip == 0) {
+        if (tireGrip == 0 || speed == 0) {
             return;
         }
 
@@ -102,7 +102,7 @@ public class Car {
     }
 
     public void moveLeft(float delta) {
-        if (tireGrip == 0) {
+        if (tireGrip == 0 || speed == 0) {
             return;
         }
 
